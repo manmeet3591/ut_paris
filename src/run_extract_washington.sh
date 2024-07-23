8a3dfc3d579b6c06045d67f62c5f7ad3336eb500
Manmeet Singh <manmeet20singh11@gmail.com>
2024-07-22 23:18:23 -0500

    Create run_extract_washington.sh

---
+++ src/run_extract_washington.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+for file in graphcast_2017_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+for file in graphcast_2018_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+for file in graphcast_2019_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+for file in graphcast_2020_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+for file in graphcast_2021_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+
+for file in graphcast_2022_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
+for file in graphcast_2023_*
+do
+echo $file
+python extract_graphcast_washington.py $file
+done
+
